69371598fcfa2fce83eaa54e78aee60bd1dc055a
Dylan Vos <dylan@revenew.nl>
2020-03-21 11:03:34 +0100

    Updated entrypoint so the zerotier network id can be loading from /home/container/zerotier

--- zerotier-entrypoint.sh
+++ zerotier-entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+file="/home/container/zerotier"
 
-# zerotier-one & export APP_PID=$!
-# sleep 5
+if [ ! -f "$file" ]; then
+    echo "Found zerotier file at $file"
+    zerotier-one &
+    export APP_PID=$!
+    echo "Zerotier starting..."
+    sleep 5
+    echo "Zerotier started - going to join networks mentioned in $file"
+    while read id; do
+        zerotier-cli join $id
+    done <"$file"
+    echo "See log above if joining was succesful"
+else
+    echo "No Zerotier file found at $file"
+fi
 
-# for ID in $(echo $NETWORK_ID | tr "," "\n")
-# do
-#     zerotier-cli join $ID
-# done
-
-# runuser -l container -c "exec $@"
 exec gosu container "$@"
