b67954927771219d9c72757fdb39d2b65c49f9ef
Dylan Vos <dylan@revenew.nl>
2020-03-21 11:22:34 +0100

    Added fix for if container is ran as non-root

--- zerotier-entrypoint.sh
+++ zerotier-entrypoint.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
-file="/home/container/zerotier"
-
-if [ ! -f "$file" ]; then
-    echo "No Zerotier file found at $file"
+if ((EUID != 0)); then
+    user="$(whoami)"
+    echo "User is non-root: $user | So I can't run Zerotier"
+    exec "$@"
 else
-    echo "Found zerotier file at $file"
-    zerotier-one &
-    export APP_PID=$!
-    echo "Zerotier starting..."
-    sleep 5
-    echo "Zerotier started - going to join networks mentioned in $file"
-    while read id; do
-        zerotier-cli join $id
-    done <"$file"
-    echo "See log above if joining was succesful"
-fi
+    export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+    file="/home/container/zerotier"
 
-exec gosu container "$@"
+    if [ ! -f "$file" ]; then
+        echo "No Zerotier file found at $file"
+    else
+        echo "Found zerotier file at $file"
+        zerotier-one &
+        export APP_PID=$!
+        echo "Zerotier starting..."
+        sleep 5
+        echo "Zerotier started - going to join networks mentioned in $file"
+        while read id; do
+            zerotier-cli join $id
+        done <"$file"
+        echo "See log above if joining was succesful"
+    fi
+
+    exec gosu container "$@"
+fi
